5da4c9e31aad8e3dca1587045bad4921df39250f
YBogdan RADOI <radoi.office@gmail.com>
2021-12-18 09:22:40 +0100

    Get user input on form submit or input changed

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,37 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredNamed] = useState("");
+
+  const nameInputChangedHandler = (event) => {
+    setEnteredNamed(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // Clear form + bind "value" in input to "enteredName"
+    setEnteredNamed("");
+    // Clear form = Not recommended bcos we manipulate the DOM, we should let React do it only
+    nameInputRef.current.value = '';
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangedHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
